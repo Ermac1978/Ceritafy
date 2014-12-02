74f60f30b645cede7f007ce279acc6144cd7121b
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-12-01 21:11:53 -0500

    first commit

---
+++ app/models/chapter.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: chapters
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Chapter < ActiveRecord::Base
+end
---
+++ test/models/story_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'test_helper'
+
+class StoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
