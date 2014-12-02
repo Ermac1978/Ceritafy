be10a3ff9cbe0970330931ea423f086f3e1b32cf
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-12-01 23:31:28 -0500

    Successfully added ActiveAdmin to app, also made sure that only user role 'admin' can only access /admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,7 @@ class ApplicationController < ActionController::Base
     current_user
   end
 
+  # For ActiveAdmin, only admin users can get to /admin
   def authenticate_admin_user!
     redirect_to root_path unless current_user.admin?
   end
