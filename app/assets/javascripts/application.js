326c54352e041490a7ec72c57a4f4ebe694dcaee
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-12-07 20:56:36 -0500

    Added nested form for Story, Chapter, Option

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var ready;
+
+ready = function() {
+    $('form').on('click', '.remove_fields', function(event) {
+        $(this).prev('input[type=hidden]').val('1');
+        $(this).closest('fieldset').hide();
+        return event.preventDefault();
+    });
+    return $('form').on('click', '.add_fields', function(event) {
+        var regexp, time;
+        time = new Date().getTime();
+        regexp = new RegExp($(this).data('id'), 'g');
+        $(this).before($(this).data('fields').replace(regexp, time));
+        return event.preventDefault();
+    });
+};
+
+$(document).ready(ready);
+
+$(document).on('page:load', ready);
