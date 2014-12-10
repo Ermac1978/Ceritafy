a696b97a25122c8fee9bb84ecf81baff83b25fda
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-12-09 19:56:41 -0500

    removed duplicate migrations

--- db/migrate/20141209190331_add_impressionable_id_to_impressions.rb
+++
@@ -1,5 +0,0 @@
-class AddImpressionableIdToImpressions < ActiveRecord::Migration
-  def change
-    add_column :impressions, :impressionable_id, :integer
-  end
-end
--- db/migrate/20141209190350_add_impressionable_type_to_impressions.rb
+++
@@ -1,5 +0,0 @@
-class AddImpressionableTypeToImpressions < ActiveRecord::Migration
-  def change
-    add_column :impressions, :impressionable_type, :string
-  end
-end
